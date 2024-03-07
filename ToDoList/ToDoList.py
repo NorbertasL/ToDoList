c83c1ddde2b3e13af085af263889632654b5cd00
NorbertasL <nlapenas@gmail.com>
2024-03-07 12:28:05 +0000

    Moved the entry logic from GUI to ToDoLogic

--- ToDoList/ToDoList.py
+++ ToDoList/ToDoList.py
@@ -1,6 +1,7 @@
 
 import tkinter as tk
 from tkinter import Toplevel, messagebox
+from types import MappingProxyType
 from typing import Callable
 from datetime import datetime
 
@@ -28,21 +29,40 @@ class ToDoEntry():
         
 
 class ToDoLogic():
-   
-    def on_new_item(self):
-        print("New Item Logic")
+    
+    def __init__(self, list_change_callback: Callable):
+        self.__entry_list: list[ToDoEntry] = []
+        self.__list_change_callback = list_change_callback
+    
+    def get_entry_display_strings(self) -> tuple[str]:
+        return_list: list[str] = []
+        for entry in self.__entry_list:
+            return_list.append(str(entry))
+        return tuple(return_list)
+    
+    def add_new_entry(self, val: MappingProxyType):
+        self.__entry_list.append(ToDoEntry(val["title"]))
+        self.__list_change_callback()
+        
+    def remove_entry(self, index: int):
+        del(self.__entry_list[index])
+        self.__list_change_callback()
+        
+    def mark_entry_done(self, index: int):
+        self.__entry_list[index].mark_done()
+        self.__list_change_callback()
+        
 
 class ToDoGUI(tk.Tk):    
-    DEFAULT_CONGIFS: dict = {
+    DEFAULT_CONGIFS: MappingProxyType = MappingProxyType({
         "root_w": 400,
         "root_h": 800,
         "root_title": "ToDo List",
-        
-        }
+        })
     
     def __init__(self):
         super().__init__()
-        self.__logic = ToDoLogic()
+        self.__logic = ToDoLogic(self.reload_list)
 
         #self.__root: tk.Tk  = tk.Tk()
         self.__create_main_window()
@@ -54,19 +74,16 @@ class ToDoGUI(tk.Tk):
         self.geometry(f"{ToDoGUI.DEFAULT_CONGIFS['root_w']}x{ToDoGUI.DEFAULT_CONGIFS['root_h']}")
        
         
-        self.__list_objects: list[ToDoEntry] = [ToDoEntry("TEST One"), ToDoEntry("TEST Two")]
-        self.__todo_list_items: tk.Variable = tk.Variable(value= self.__list_objects)
-        self.__listbox: tk.Listbox = tk.Listbox(
-            self,
-            listvariable=self.__todo_list_items
-            )
+        #self.__list_objects: list[ToDoEntry] = [ToDoEntry("TEST One"), ToDoEntry("TEST Two")]
+        #self.__todo_list_items: tk.Variable = tk.Variable(value= self.__list_objects)
+        self.__listbox: tk.Listbox = tk.Listbox(self)
         self.__listbox.configure(font=('Helvetica', 12))
         self.__listbox.bind('<Double-1>', self.__on_even_doubleclick)
         self.__listbox.bind('<Button-3>', self.__on_even_rightclick)
         self.__listbox.pack()
        
         
-        self.update()
+        self.reload_list()
         
         
         
@@ -78,40 +95,49 @@ class ToDoGUI(tk.Tk):
             ).pack()
         
     def __on_even_doubleclick(self, event:tk.Event):
+        
+        #prvents errors when clicking empty box
         if(len(self.__listbox.curselection()) == 0):
             return
+        
         index:int = event.widget.curselection()[0]
-        print(f"Double Click on Event Index:{index} Object:{self.__list_objects[index]}")
-        self.__list_objects[index].mark_done()
-        self.update()
+        self.__logic.mark_entry_done(index)
+
+ 
         
     def __on_even_rightclick(self, event: tk.Event):
         self.__listbox.selection_clear(0,tk.END) # clears focus so we can only select one
         self.__listbox.selection_set(self.__listbox.nearest(event.y)) # selects the element
+        #prvents errors when clicking empty box
         if(len(self.__listbox.curselection()) == 0):
             return
         index:int = self.__listbox.curselection()[0]
-        print(f"Right Click on Event Index:{index} Object:{self.__list_objects[index]}")
         self.__on_delete_entry(index)
         
         
-    def __on_new_entry(self, entry: ToDoEntry):
-        self.__list_objects.append(entry)
-        self.update()
+    def __on_new_entry(self, entry: MappingProxyType):
+        self.__logic.add_new_entry(entry)
         
     def __on_delete_entry(self, index: int):
-        if messagebox.askyesno("", f"Are You sure you want to delete {self.__list_objects[index]}?"):
-            del(self.__list_objects[index])
-            self.update()
+        if messagebox.askyesno("", f"Are You sure you want to delete {self.__listbox.get(index)}?"):
+            self.__logic.remove_entry(index)
 
-    def update(self):
-        self.__todo_list_items.set(self.__list_objects)
-        self.__listbox.configure(height = len(self.__list_objects))
+
+    def reload_list(self):
+        #self.__listbox.destroy()
+        self.__listbox.delete(0, tk.END)
+        self.__listbox.insert(tk.END, *self.__logic.get_entry_display_strings())
+        self.__listbox.config(height = self.__listbox.size())
+        #self.__todo_list_items.set(self.__list_objects)
+        #self.__listbox.configure(height = len(self.__list_objects))
         
     def __add_todo(self):
-        def show_add_windows(on_new_entry: Callable):
+        def show_add_windows():
             def on_save():
-                on_new_entry(ToDoEntry(title_var.get(), info_var.get())) 
+                self.__on_new_entry(MappingProxyType({"title": title_var.get(), 
+                                                      "info": info_var.get()}
+                                                     )
+                                    ) 
                 add_task_widnow.destroy()
                 
             add_task_widnow: Toplevel = Toplevel(self)
@@ -149,10 +175,8 @@ class ToDoGUI(tk.Tk):
             btn_cancel.pack()
             
         
-        show_add_windows(self.__on_new_entry)
+        show_add_windows()
         
-        
-        self.__logic.on_new_item()
         
     def start(self):
         self.mainloop()
