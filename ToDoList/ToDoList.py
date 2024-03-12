0f8dd1d8a86afdbb44f9a11d7fd42aa5519c136e
NorbertasL <nlapenas@gmail.com>
2024-03-12 15:27:23 +0000

    Cleaned up and commeted code

--- ToDoList/ToDoList.py
+++ ToDoList/ToDoList.py
@@ -1,47 +1,96 @@
-
-from sre_constants import SUCCESS
 import tkinter as tk #GUI
 from tkinter import Button, Toplevel, Listbox, messagebox
 from types import MappingProxyType #MappingProxyType is a immutable dictionary
 from typing import Callable # for hinting a fucntion as parameter
 from datetime import datetime
-import sqlite3 # local database
+import sqlite3# local database
 
 class ToDoEntry():
     def __init__(self, id:int, date:str, title: str, info:str = "", isDone:bool = False):
+        '''
+        Args:
+            id(int): local db _ID
+            date(str): entry creation date
+            title(str)
+            info(str): extra information
+        '''
         self.__id = id
         self.__creation_date: str = date
         self.__title: str = title
-        self.__info: str = info 
+        self.__info: str = info  
         self.__is_done = isDone
         
     def get_id(self) -> int:
+        '''
+        Getter for id
+        
+        Returns:
+            int: local databse id
+        '''
         return self.__id
         
     def get_title(self) -> str:
+        '''
+        Getter for title
+        
+        Returns:
+            str
+        '''
         return self.__title
     
     def get_info(self) -> str:
+        '''
+        Getter for info
+        
+        Returns:
+            str
+        '''
         return self.__info
     
     def __str__(self) -> str:
+        '''
+        override to string method 
+        using it for GUI display string
+        
+        Returns:
+            str: title string , crossed out if is_done set to true
+        '''
         temp:str = f"{self.__title}"
         if self.__is_done:
            temp ='\u0337'+ '\u0337'.join(temp)+'\u0337'
         return temp
-    
-    def mark_done(self):
-        self.__is_done = True
         
     def is_done(self) -> bool:
+        '''
+        Getter for completed stated
+        
+        Returns:
+            bool
+        '''
         return self.__is_done
         
 class Database_Manager:
+    '''
+    Statsic class that manages the local database(SQLite) connection/querries
+    '''
     DATABASE_PATH:str = "ToDoDatabase.db"
     MAIN_TABLE:str = "Entries"
 
     @classmethod
     def execure_sql_querry(cls, sql_statment:str, write_querry:bool = False, values:tuple = ()) -> tuple[bool, list]: # list | bool Python 3.10+ Hinting
+        '''
+        Connects to the local database and executes the querrrie
+        
+        Args:
+           sql_statment(str): SQL Querrie
+           write_querry(bool): set true if its a write function
+           values(tuple): write function might have values that need to be passed it
+           
+        Returns:
+            tuple(bool, list)
+                bool: was the querrie sucsessful?
+                list: get(non-write) querries return a list of rows
+        '''
         successful: bool = False 
         return_list: list = []
         try:
@@ -61,11 +110,27 @@ class Database_Manager:
         
     @classmethod
     def verify_db(cls) -> bool:
+        '''
+        Checks if database and table exists, if not it creates them
+        
+        Retruns:
+            bool: connection good?
+        '''
         main_table_sql:str = f'''CREATE TABLE IF NOT EXISTS {cls.MAIN_TABLE}(_ID INTEGER PRIMARY KEY,date TEXT, title TEXT,info TEXT,is_done BOOL);'''  
         return cls.execure_sql_querry(main_table_sql, True)[0] # only returning the state bool
     
     @classmethod
     def add_entry(cls, val: MappingProxyType) -> bool:
+        '''
+        Allows to add new entry to local db
+        
+        Args:
+           val(MappingProxyType): immutable dic of keys(columbs in db) and values
+           
+        Returns:
+            bool: entry sucsesfull
+        '''
+
         #generates a INSERT querry with dic keys as keys for db columbs and values as ?. This allows me to pass values in @call and
         #has a built int querry sanitization to prevent SQL injections
         sql_querry:str = f"INSERT INTO {cls.MAIN_TABLE}({','.join(val.keys())}) VALUES({','.join('?'*len(val))})" 
@@ -73,22 +138,52 @@ class Database_Manager:
     
     @classmethod
     def delete_entry(cls, id:int) -> bool:
+        '''
+        Allows to remove entry to local db
+        
+        Args:
+           id(int): local database id
+           
+        Returns:
+            bool: delete sucsesfull
+        '''
+        
         sql_querry:str = f"DELETE FROM {cls.MAIN_TABLE} WHERE _ID={id};"
         return cls.execure_sql_querry(sql_querry, True)[0]
     
     @classmethod
     def get_entries(cls) -> tuple[bool, list]:
+        '''
+        Gets all the entires from the local database
+        
+        Returns:
+            tuple(bool, list)
+                bool: was the get sucsesfull
+                list: list of rows
+        '''
         sql_querry:str = f"SELECT * FROM {cls.MAIN_TABLE};"
         return cls.execure_sql_querry(sql_querry)
     
     @classmethod
     def change_done_status(cls, id: int, status: bool = True) -> bool:
+        '''
+        Changes the complete status of an entry
+           
+        Args:
+            id(int): local database entry(row) id
+            statsus(bool): new status of the entry
+        '''
         sql_querry:str = f"UPDATE {cls.MAIN_TABLE} SET is_done={status} WHERE _ID={id};"
         return cls.execure_sql_querry(sql_querry, True)[0]
   
 class ToDoLogic():
     
     def __init__(self, list_change_callback: Callable, error_callback: Callable):
+        '''
+        Args:
+            list_change_callback(Callable): callback for GUI to update its display list
+            error_callback(Callable): callback for GUI to display an error message
+        '''
         self.__entry_list: list[ToDoEntry] = []
         self.__list_change_callback = list_change_callback
         self.__error_callback = error_callback # method that displays an error box with a custom message
@@ -97,32 +192,71 @@ class ToDoLogic():
            
           
     def get_entry_display_strings(self) -> tuple[str]:
+        '''
+        get a string list representation of entry_list objects
+        
+        Returns:
+            tuple[str]: each elements represent a ToDoEntry Object
+        '''
         return_list: list[str] = []
         for entry in self.__entry_list:
             return_list.append(str(entry))
         return tuple(return_list)
     
     def add_new_entry(self, val: MappingProxyType):
-        #self.__entry_list.append(ToDoEntry(val["title"]))
+        '''
+        Allows to add new ToDo entry
+        
+        Args:
+           val(MappingProxyType): immutable dic of keys(columbs in db) and values
+        '''  
+    
         if not Database_Manager.add_entry(val):
             self.__error_callback("Failed To Add Entry")
         self.update_entry_list()
         
         
     def remove_entry(self, index: int):
+        '''
+        Removes entry from ToDo based on the index in listbox
+        
+        Args:
+            index(int): index in the list box
+        '''
         if not Database_Manager.delete_entry(self.__entry_list[index].get_id()):
             self.__error_callback("Failed To Remove Entry")
         self.update_entry_list()
         
     def change_entry_status(self, index: int, status: bool):
+        '''
+        Change complete status for ToDo entry
+        
+        Args:
+            index(int): index of the entry in the listbox
+            status(bool): new status for the entry
+        '''
         if not Database_Manager.change_done_status(self.__entry_list[index].get_id(), status):
             self.__error_callback("Failed To Update Entry")
         self.update_entry_list()
         
     def get_done_status(self, index: int) -> bool:
+        '''
+        returns the completeion statsu for the entry
+        
+        Args:
+            index(int): index of the entry in listbox
+        
+        Returns:
+            bool: staus of the entry(complete(True) or not(False))
+        '''
         return self.__entry_list[index].is_done()
 
     def update_entry_list(self):
+        '''
+        Updates the entry_list of ToDoEntry objects 
+        AND GUI display in the listbox
+        '''
+
         self.__entry_list = [] #reseting it
         successful,  entries = Database_Manager.get_entries()
         if not successful:
@@ -133,10 +267,13 @@ class ToDoLogic():
              self.__entry_list.append(ToDoEntry(*entry))
         self.__list_change_callback()
         
-class ToDoGUI(tk.Tk):    
+class ToDoGUI(tk.Tk):   
+    # Some default configs to be used in the GUI
     DEFAULT_CONGIFS: MappingProxyType = MappingProxyType({
         "root_w": 400,
         "root_h": 500,
+        "add_box_w": 200,
+        "add_box_h": 200,
         "root_title": "ToDo List",
         "main_font" : ("Helvetica", 25),
         "main_colour": "#ffd900",
@@ -145,16 +282,25 @@ class ToDoGUI(tk.Tk):
         }) 
     
     def __init__(self):
-        super().__init__()
+        super().__init__()#Tk parent class init
+        
         self.__logic = ToDoLogic(self.reload_list, self.error_msgbox)
 
-        #self.__root: tk.Tk  = tk.Tk()
         self.__create_main_window()
-        self.__offset_x = 0
+        
+        # Windows opens in the top left of screen.
+        self.__offset_x = 0 
         self.__offset_y = 0
     
-    def error_msgbox(self, msg):
-        messagebox.showerror("Error!", msg)
+    def error_msgbox(self, msg:str, title:str = "Error!"):
+        '''
+        Easy way to display and error message in the GUI
+        
+        Args:
+            msg(str): message to display in the error box
+            title(srg): OPTIONAL Error Box Title DEFAULT = Error!
+        '''
+        messagebox.showerror(title, msg)
     def __create_main_window(self):
         def create_custom_title_bar():
             def move_window(event):#allows me to move window
@@ -255,12 +401,19 @@ class ToDoGUI(tk.Tk):
 
 
     def reload_list(self):
+        '''
+        Reload the GUI ListBox display data
+        Gets the data from ToDoLogic
+        '''
+
         self.__listbox.delete(0, tk.END) #clears the list
         for entry in self.__logic.get_entry_display_strings():       
             self.__listbox.insert(tk.END, entry)
         
         #Figures out the size of the list_box
         list_box_size: int = self.__listbox.size()
+        
+        # Makig sure not to go over max size or the listbox pushes out the +(add) button out of view
         if list_box_size > ToDoGUI.DEFAULT_CONGIFS["list_box_row_max"]:
             list_box_size = ToDoGUI.DEFAULT_CONGIFS["list_box_row_max"]        
         self.__listbox.config(height = list_box_size)
@@ -268,50 +421,60 @@ class ToDoGUI(tk.Tk):
     def __add_todo(self):
         def show_add_windows():
             def on_save():
-                self.__on_new_entry(MappingProxyType({"title": title_var.get(), 
-                                                      "info": info_var.get(),
-                                                      "date": datetime.now(),
-                                                      "is_done": False}
+                '''
+                Called when we want to save the info the the add box
+                '''
+                self.__on_new_entry(MappingProxyType({"title": title_var.get(), #Getting text from box var
+                                                      "info": info_var.get(), #Getting text from box var
+                                                      "date": datetime.now(), #Time when the task was created
+                                                      "is_done": False} #defaul ToDo is NOT done
                                                      )
                                     ) 
                 add_task_widnow.destroy()#data submited so we are destoying the windows
+             
                 
             add_task_widnow: Toplevel = Toplevel(self)
             add_task_widnow.title("New ToDo!")
             
-            add_task_widnow.geometry('%dx%d+%d+%d' % (100, 200, self.winfo_pointerx()-100/2, self.winfo_pointery()-50))
+            #add task window and possitions it relative to root(main) window
+            add_task_widnow.geometry('%dx%d+%d+%d' % (self.DEFAULT_CONGIFS["add_box_w"], 
+                                                      self.DEFAULT_CONGIFS["add_box_h"], 
+                                                      self.winfo_x() + self.DEFAULT_CONGIFS["root_w"] - (self.DEFAULT_CONGIFS["add_box_w"]*1.5), 
+                                                      self.winfo_y() + self.DEFAULT_CONGIFS["root_h"] - (self.DEFAULT_CONGIFS["add_box_h"]*1.5)
+                                                      )
+                                     )
             
             add_task_widnow.attributes('-topmost', True)#make it alwasy on top
+            #add_task_widnow.config(bg=ToDoGUI.DEFAULT_CONGIFS["main_colour"]) 
             
-            add_task_widnow.grab_set()#lock all othe window interactions
+            add_task_widnow.grab_set()#lock all other window interactions
             
+            #var to store the text in title
             title_var: tk.StringVar = tk.StringVar(add_task_widnow)
             title = tk.Entry(add_task_widnow, textvariable = title_var)
             
             title.insert(0, "Title")
             title.pack()
             
+            #var to store the text in info
             info_var: tk.StringVar = tk.StringVar(add_task_widnow)
             info = tk.Entry(add_task_widnow, textvariable = info_var)
             info.insert(0, "Extra Info!")
             info.pack()
             
-            btn_save = Button(add_task_widnow, 
-                                 text = "Save", 
-                                 command = on_save                                
-                        )
-            
-            btn_save.pack()            
+            #Save and Cancel buttons are not used anywhere else, so dont need them in varibales
+            Button(add_task_widnow, 
+                   text = "Save", 
+                   command = on_save
+                   ).pack()
+             
 
-            btn_cancel = Button(add_task_widnow, 
-                                   text = "Cancel",
-                                   command = add_task_widnow.destroy
-                        )
-            
-            btn_cancel.pack()
-            
-        
-        show_add_windows()
+            Button(add_task_widnow, 
+                   text = "Cancel",
+                   command = add_task_widnow.destroy
+                   ).pack()
+  
+        show_add_windows()#creates/displays the add window
         
         
     def start(self):
